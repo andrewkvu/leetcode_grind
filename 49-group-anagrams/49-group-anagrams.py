11650e0dc78c4c5eb0394ed88b2f7c05398d8ddf
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-06-02 02:14:46 -0700

    Time: 140 ms (51.59%), Space: 17.2 MB (88.66%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -5,5 +5,7 @@ class Solution:
         for string in strs:
             key = "".join(sorted(string)) # converts sorted(string) list into string key
             strDict[key] = strDict.get(key, []) + [string]
+            # sets the initial value as an empty [] but then adds the array string version
+            # similar to the .get(char, 0) + 1 but with a []
         
         return list(strDict.values())
