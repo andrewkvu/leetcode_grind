ac397712113871d666e81e5ced6d724f682fc400
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-06-01 22:43:17 -0700

    Time: 60 ms (15.01%), Space: 13.9 MB (69.64%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parMap = {'}': '{', ')': '(', ']': '['}
+        stack = []
+        for char in s:
+            if char in parMap: # if the char is a closing par
+                # check if stack is empty or if the last char is an opening par
+                if stack == [] or parMap[char] != stack.pop():
+                    return False
+            else: # if the char is an opening par
+                stack.append(char)
+        
+        return len(stack) == 0
