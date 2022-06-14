c81afc453450fc8ed9e3f729068f68d9934771e5
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-06-13 19:24:00 -0700

    Time: 129 ms (72.64%), Space: 16 MB (46.28%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -10,11 +10,11 @@ class Solution:
             if maxLeft < maxRight:
                 left += 1
                 maxLeft = max(maxLeft, height[left])
-                rain = min(maxLeft, maxRight) - height[left]
+                rain = maxLeft - height[left]
             else:
                 right -= 1
                 maxRight = max(maxRight, height[right])
-                rain = min(maxLeft, maxRight) - height[right]
+                rain = maxRight - height[right]
             if rain > 0:
                 res += rain
                 
