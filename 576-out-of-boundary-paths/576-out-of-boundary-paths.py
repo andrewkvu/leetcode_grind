a356fecbfe57c41e2c113eb4830b944e37a624d0
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-07-15 22:29:23 -0700

    Time: 144 ms (78.26%), Space: 17.6 MB (59.08%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -1,15 +1,17 @@
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-        MOD = 10 ** 9 + 7
+        # modification of brute force basically        
+        # 3D DP declaration
         dp = [([[-1] * (maxMove + 1) for i in range(n + 1)]) for j in range(m + 1)]
         
         def find(maxMove, startRow, startColumn):
             if startRow < 0 or startColumn < 0 or startRow >= m or startColumn >= n:
-                return 1
+                return 1 # out of bounds = 1
             if maxMove == 0:
-                return 0
-            if dp[startRow][startColumn][maxMove] != -1:
-                return dp[startRow][startColumn][maxMove]
+                return 0 # out of moves and its in bounds = can't do any path
+            if dp[startRow][startColumn][maxMove] != -1: # if the initial -1 changed
+                return dp[startRow][startColumn][maxMove] # don't recalculate
+            # brute force graph movement stored into the DP array
             dp[startRow][startColumn][maxMove] = \
                (find(maxMove - 1, startRow + 1, startColumn) \
             +   find(maxMove - 1, startRow - 1, startColumn) \
@@ -18,5 +20,6 @@ class Solution:
             
             return dp[startRow][startColumn][maxMove]
         
+        MOD = 10 ** 9 + 7
         return find(maxMove, startRow, startColumn) % MOD
             
