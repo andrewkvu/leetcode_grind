f61b42c8d4e4c4ddae1b171c0bbb3cb30028c60a
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-07-27 17:07:41 -0700

    Time: 1103 ms (72.34%), Space: 68.8 MB (31.45%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,22 +1,30 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # marking initial negs to 0s
-        for i in range(len(nums)):
-            if nums[i] < 0:
-                nums[i] = 0
+#         # marking initial negs to 0s
+#         for i in range(len(nums)):
+#             if nums[i] < 0:
+#                 nums[i] = 0
         
-        for i in range(len(nums)):
-            val = abs(nums[i])
-            if 1 <= val <= len(nums):
-                if nums[val - 1] > 0: # mark negative to parallel actions of a hashset
-                    nums[val - 1] *= -1
-                elif nums[val - 1] == 0: # 0 means was negative, mark with a special "i dont care"
-                    nums[val - 1] = -1 * (len(nums) + 1)
+#         for i in range(len(nums)):
+#             val = abs(nums[i])
+#             if 1 <= val <= len(nums):
+#                 if nums[val - 1] > 0: # mark negative to parallel actions of a hashset
+#                     nums[val - 1] *= -1
+#                 elif nums[val - 1] == 0: # 0 means was negative, mark with a special "i dont care"
+#                     nums[val - 1] = -1 * (len(nums) + 1)
+        
+#         for i in range(1, len(nums) + 1):
+#             if nums[i - 1] >= 0: # number is positive = thats the value in the hashset that it wouldve been
+#                 return i
+#         return len(nums) + 1 # worst case if all the numbers are positive
+
+        s = set(nums)
         
         for i in range(1, len(nums) + 1):
-            if nums[i - 1] >= 0: # number is positive = thats the value in the hashset that it wouldve been
+            if i not in s:
                 return i
-        return len(nums) + 1 # worst case if all the numbers are positive
+        
+        return len(nums) + 1
             
         
         
