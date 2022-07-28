29e450ca8962c1b8b07c6e8234472487043c0cb5
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-07-27 17:05:41 -0700

    Time: 1257 ms (57.01%), Space: 63.3 MB (35.77%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,5 +1,6 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        # marking initial negs to 0s
         for i in range(len(nums)):
             if nums[i] < 0:
                 nums[i] = 0
@@ -7,15 +8,15 @@ class Solution:
         for i in range(len(nums)):
             val = abs(nums[i])
             if 1 <= val <= len(nums):
-                if nums[val - 1] > 0:
+                if nums[val - 1] > 0: # mark negative to parallel actions of a hashset
                     nums[val - 1] *= -1
-                elif nums[val - 1] == 0:
+                elif nums[val - 1] == 0: # 0 means was negative, mark with a special "i dont care"
                     nums[val - 1] = -1 * (len(nums) + 1)
         
         for i in range(1, len(nums) + 1):
-            if nums[i - 1] >= 0:
+            if nums[i - 1] >= 0: # number is positive = thats the value in the hashset that it wouldve been
                 return i
-        return len(nums) + 1
+        return len(nums) + 1 # worst case if all the numbers are positive
             
         
         
