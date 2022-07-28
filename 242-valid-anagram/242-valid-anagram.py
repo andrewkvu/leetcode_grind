dde2052582cff79dcee2a9b06202198fd5435c84
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-07-27 17:11:09 -0700

    Time: 51 ms (90.55%), Space: 14.3 MB (96.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -3,23 +3,4 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        charMapS = {}
-        charMapT = {}
-        
-        
-        # for char in s:
-        #     charMapS[char] = charMapS.get(char, 0) + 1
-        # for char in t:
-        #     charMapT[char] = charMapT.get(char, 0) + 1
-            
-        # also the same as 
-        for i in range(len(s)):
-            charMapS[s[i]] = charMapS.get(s[i], 0) + 1
-            charMapT[t[i]] = charMapT.get(t[i], 0) + 1
-        
-        for char in charMapS:
-            if charMapS[char] != charMapT.get(char, 0):
-                return False
-            
-        return True
-            
+        return Counter(s) == Counter(t)
