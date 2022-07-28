a3b427db43006f24b679559c24fcc1dcedfaacdb
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-07-27 17:50:43 -0700

    Time: 157 ms (50.97%), Space: 15.7 MB (84.21%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -4,11 +4,14 @@ class Solution:
         ROWS = len(dp)
         COLS = len(dp[0])
         
-        for i in range(1, ROWS):
+        # start at 1 because [0][0] is already good
+        for i in range(1, ROWS): # do first column
             dp[i][0] += dp[i - 1][0]
-        for j in range(1, COLS):
+        for j in range(1, COLS): # do first row
             dp[0][j] += dp[0][j - 1]
         
+        # compute for the rest of the squares since now you have all starting paths
+        # from top left to bottom right
         for i in range(1, ROWS):
             for j in range(1, COLS):
                 dp[i][j] += min(dp[i][j - 1], dp[i - 1][j])
