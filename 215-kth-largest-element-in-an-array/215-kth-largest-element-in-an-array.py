805004bd6a779d700725e572a8218f409fba9810
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-08-02 14:42:15 -0700

    Time: 672 ms (26.07%), Space: 27.1 MB (14.18%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -4,10 +4,18 @@ class Solution:
         # return nums[-k]
         
         # with heapq:
-        heap = []
-        for num in nums:
-            heapq.heappush(heap, num)
-            if len(heap) > k:
-                heapq.heappop(heap)
-        # [3] [2,3] [1,3,2] [2,3] [2,3,5] [3,5] [3,5,6] [5,6] [4,6,5] [5,6]
-        return heap[0]
+        # heap = []
+        # for num in nums:
+        #     heapq.heappush(heap, num)
+        #     if len(heap) > k:
+        #         heapq.heappop(heap)
+        # # [3] [2,3] [1,3,2] [2,3] [2,3,5] [3,5] [3,5,6] [5,6] [4,6,5] [5,6]
+        # return heap[0]
+        
+        heap = [-num for num in nums]
+        heapq.heapify(heap)
+        
+        for i in range(k):
+            ans = heapq.heappop(heap)
+        
+        return -ans
