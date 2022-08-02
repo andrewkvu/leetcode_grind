5e926ca17f52ca2ead33312663daaa4ade543917
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-08-02 15:45:11 -0700

    Time: 159 ms (88.16%), Space: 18.1 MB (85.07%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,19 +1,13 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # if the prevEnd, intervals[i][-1] >= intervals[i+1][0],
-        # the new interval is intervals[i][0], intervals[i+1][-1]
-        
-        # where x is each individual array of 2
-        intervals.sort(key = lambda x: (x[0]))
         res = []
+        intervals.sort(key = lambda x: x[0]) # sort by the beginning of each interval
         
         for interval in intervals:
-            # if res is null or if the end of res's last element is less 
-            # than this interval's beginning ([1, 2]) --> [3, 5] 
-            # where 2 is res[-1][-1] and interval[0] = 3
+            # normal case: if res is empty or current end in res is less than beginning of curr interval
             if not res or res[-1][-1] < interval[0]:
                 res.append(interval)
-            # ([1,4][2,5])
-            else: # to merge, take max of the last element's end and current end and change res's ending
+            else: # merge intervals
                 res[-1][-1] = max(res[-1][-1], interval[-1])
+        
         return res
