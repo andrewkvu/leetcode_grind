344fc9053943784f6b4a5c8f55bbffafa52b3c68
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-08-19 00:28:19 -0700

    Time: 298 ms (64.16%), Space: 15.7 MB (75.38%) - LeetHub

--- 846-hand-of-straights/846-hand-of-straights.py
+++ 846-hand-of-straights/846-hand-of-straights.py
@@ -6,10 +6,10 @@ class Solution:
         have = Counter(hand)
         
         for num in have:
-            if have[num] > 0:
-                need = have[num]
-                for i in range(num, num + groupSize):
-                    if have[i] < need:
+            if have[num] > 0: # if you still have some of this card
+                need = have[num] # use this as the base of your straight
+                for i in range(num, num + groupSize): # straight from num to num + groupSize
+                    if have[i] < need: # if you have less than you need, false
                         return False
-                    have[i] -= need
+                    have[i] -= need # remember to subtract out the ones you already used
         return True
