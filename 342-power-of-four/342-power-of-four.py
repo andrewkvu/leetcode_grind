92c4547b7359dc93cf9d5cbcecc41e1c8d53b4d2
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-08-21 17:14:18 -0700

    Time: 57 ms (35.23%), Space: 13.8 MB (54.38%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -3,6 +3,6 @@ class Solution:
         while n > 1:
             if n % 4 == 0:
                 n = n // 4
-            else:
-                return False
-        return True if n == 1 else False
+            else: return False
+        
+        return n == 1
