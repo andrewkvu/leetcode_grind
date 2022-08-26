ab4e1a2a5fde46c8190c99ad365c893cf19b8ca0
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-08-25 17:21:29 -0700

    Time: 57 ms (59.89%), Space: 13.8 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,8 +1,7 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        return sorted(str(n)) in [sorted(str(1 << i)) for i in range(30)]
         powers = []
         for i in range(30):
             powers.append(sorted(str(1 << i)))
         
-        return sorted(str(N)) in powers
+        return sorted(str(n)) in powers
