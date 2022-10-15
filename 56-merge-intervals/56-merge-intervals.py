f1d0e1e654d217136f61f923158c305f99db1d8e
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-10-15 00:36:20 -0700

    Time: 304 ms (46.95%), Space: 18.1 MB (53.35%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,13 +1,22 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # make res array for output
         res = []
-        intervals.sort(key = lambda x: x[0]) # sort by the beginning of each interval
-        
+        # sort list of intervals by first
+        intervals.sort(key = lambda x: x[0])
+        # loop through each interval of the array
         for interval in intervals:
-            # normal case: if res is empty or current end in res is less than beginning of curr interval
-            if not res or res[-1][-1] < interval[0]:
+        # 2 cases:
+        # case 1: new interval starts at a normal time - if its the first interval OR if this interval's beginning is after the last interval's end
+            if not res or interval[0] > res[-1][-1]:
+            # append interval
                 res.append(interval)
-            else: # merge intervals
+        # case 2: new interval starts in between the previous interval's end time
+            else:
                 res[-1][-1] = max(res[-1][-1], interval[-1])
-        
+            # set the current last interval in the res array to the max of the current last interval's end
+            # and the new interval's last end
+            
+        # then return result
         return res
+        
