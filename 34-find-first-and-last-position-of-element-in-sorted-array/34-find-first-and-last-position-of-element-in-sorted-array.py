010cdecbfc1df367a2ae03de60262ea3febe7c24
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-10-20 12:15:07 -0700

    Time: 203 ms (23.26%), Space: 15.4 MB (49.40%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,39 +1,21 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        res = [self.findFirst(nums, target), self.findLast(nums, target)]
-        return res
+        first = self.binarySearch(nums, target)
+        last = self.binarySearch(nums, target + 1)
         
-    def findFirst(self, nums, target):
+        if first == len(nums) or nums[first] != target:
+            return [-1, -1]
+        return [first, last - 1]
+        
+    def binarySearch(self, nums, target):
         lo = 0
-        hi = len(nums) - 1
-        index = -1
+        hi = len(nums)
         
-        while lo <= hi:
+        while lo < hi:
             mid = (lo + hi) // 2
-            if nums[mid] == target:
-                index = mid
-                hi = mid - 1
-            elif nums[mid] > target:
-                hi = mid - 1
+            if nums[mid] < target:
+                lo = mid + 1
             else:
-                lo = mid + 1
-        
-        return index
-    
-    
-    def findLast(self, nums, target):
-        lo = 0
-        hi = len(nums) - 1
-        index = -1
-        
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if nums[mid] == target:
-                index = mid
-                lo = mid + 1
-            elif nums[mid] > target:
-                hi = mid - 1
-            else:
-                lo = mid + 1
-                
-        return index
+                hi = mid
+            
+        return lo
