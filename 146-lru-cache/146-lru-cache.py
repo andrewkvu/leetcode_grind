2fb301ea87ecec77146d83d4f829ae1739067dfd
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-10-29 10:16:37 -0700

    Time: 1977 ms (41.77%), Space: 76.6 MB (23.28%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,61 +1,71 @@
-# left and right nodes
-# left = least recently used
-# right = most recently used
-# doubly linked list, hashmap
+# make a node class for doubly linked list
 
 class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev = self.next = None
+        self.next = None
+        self.prev = None
 
 
 class LRUCache:
 
-    def __init__(self, capacity: int):
-        self.cap = capacity
+    # initialize with cache, capacity, dummy nodes left and right, then link them together doubly linked
+    def __init__(self, capacity):
         self.cache = {}  # key : node
-        # initialized as dummies
-        self.left = Node(0, 0)  # least recently used node
-        self.right = Node(0, 0)  # most recently used node
-        # .next and .prev are the real values
-        self.left.next = self.right  # link them together
+        self.cap = capacity
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+        self.left.next = self.right
         self.right.prev = self.left
 
-    # remove node from list doubly linked list
-    # draw the pictures for both remove and insert
+    # remove any node
+    # O -> 1 -> 2
+    #   <-   <-
+    # O -> 2
+    #   <-
     def remove(self, node):
         nxt = node.next
         previous = node.prev
 
         previous.next = nxt
         nxt.prev = previous
-
-    # insert node at right
+    # insert at the right
+    # 0 -> 2
+    #   <-
+    # 0 -> 1 -> 2
+    #   <-   <-
     def insert(self, node):
         previous = self.right.prev
         nxt = self.right
 
         previous.next = node
-        nxt.prev = node
         node.next = nxt
+        nxt.prev = node
         node.prev = previous
 
-    def get(self, key: int) -> int:
+    # return value of key if key exists
+    # if key exists, (update and return value since you are using it) remove and insert at the right to show most recently used
+    # else return -1
+    def get(self, key):
         if key in self.cache:
-            self.remove(self.cache[key])  # remove and insert at the right to show
-            self.insert(self.cache[key])  # most recently used node
-            return self.cache[key].val  # self.cache[key] returns node, .val returns real val
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
         else:
             return -1
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:  # node already exists in list with same key/val
+    # update value of key if exists
+    # if key in cache, remove it from cache, replace spot in cache with separate node for it and insert that into the doubly linked list
+    # check if number of keys is more than capacity, the LRU is the left.next since left is a dummy, then delete the lru node with the lru key
+    def put(self, key, value):
+        if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
 
-        if len(self.cache) > self.cap:  # remove from list and delete LRU from hashmap
-            lru = self.left.next  # always LRU
+        if len(self.cache) > self.cap:
+            lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+
