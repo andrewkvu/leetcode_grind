8996d310ea959ea48a7dd5591668a1b0a78dc28b
Andrew Vu <68661570+andrewkvu@users.noreply.github.com>
2022-10-30 01:05:26 -0700

    Time: 679 ms (15.65%), Space: 16.4 MB (6.98%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.py
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -1,29 +1,26 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        # initialize array of -1's
+        # initialize res array with -1's
+        # stack with [index, nums[i]]
+        # for each value, check if the stack is nonempty and check if this value 
+        # is greater than the top element of the stack
+        # while it is, keep popping from the stack and 
+        # then set the position in res to that nums[i] value
+        # do twice in order to get circular
+        
         res = [-1] * len(nums)
-        # initialize stack
-        stack = []
-        currMax = 0
-        # loop over values in nums
+        stack = [] # [index, value]
+        
         for i in range(len(nums)):
-        # while stack is nonempty and current value in nums is greater than the top of the stack
             while stack and nums[i] > stack[-1][-1]:
-                # pop from the stack
-                stackIndex, stackVal = stack.pop()
-                # change the value from the popped stack into res
+                stackIndex, stackValue = stack.pop()
                 res[stackIndex] = nums[i]
-            # append to the stack
             stack.append([i, nums[i]])
             
         for i in range(len(nums)):
-        # while stack is nonempty and current value in nums is greater than the top of the stack
             while stack and nums[i] > stack[-1][-1]:
-                # pop from the stack
-                stackIndex, stackVal = stack.pop()
-                # change the value from the popped stack into res
+                stackIndex, stackValue = stack.pop()
                 res[stackIndex] = nums[i]
-            # append to the stack
             stack.append([i, nums[i]])
-        # return res
+        
         return res
